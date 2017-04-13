c76cb39ca7ea419843279ba78ac9b3325843b630
Ubuntu <ubuntu@ip-172-31-17-68.us-west-2.compute.internal>
2017-04-13 19:10:05 +0000

    test crawl

--- exampleCrawler/crawler.js
+++ exampleCrawler/crawler.js
@@ -1,5 +1,23 @@
+/* Libraries */
 var request = require('request');	//make HTML requests
 var cheerio = require('cheerio'); 	//parse HTML elements
 var URL = require('url-parse');		//parse URL
+
+/* Fetching and parsing a web page in Javascript */
+var pageToVisit = "52.89.243.4";
+console.log("Visiting page " + pageToVisit);
+request(pageToVisit, function(err, res, body){
+	if(err){
+		console.log("Error: " + err);
+	}
+	//Check status code
+	console.log("Status code: " + res.statusCode);
+	if(res.statusCode === 200){
+		//status code is good- being parsing DOM
+		var $ = cheerio.load(body);
+		console.log("Page title: " + $('title').text());
+	}
+};
+
 
 
